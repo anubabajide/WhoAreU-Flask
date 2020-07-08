7e3a24feb1af202b320a5f2f2c5b67de245a2de8
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-07-08 12:39:12 +0100

    remove pycache

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+import os
+from flask import Flask, abort, request, jsonify, g, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+import jwt
+from werkzeug.security import generate_password_hash, check_password_hash
 
+#Initialize variables
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'use a random string to construct the hash'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+# Extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 @app.route('/')
 def hello():
